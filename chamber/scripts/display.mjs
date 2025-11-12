9893dd48e5e53cb3174368d4a166175550aede48
SarahMecham <sarahdmecham@gmail.com>
2025-11-11 20:32:26 -0700

    Added css styling to home page

--- chamber/scripts/display.mjs
+++ chamber/scripts/display.mjs
@@ -1,28 +1,43 @@
 export function createBusinessCard(business) {
-  const card = document.createElement("section");
-  card.classList.add("card");
+    const card = document.createElement("section");
+    card.classList.add("card");
 
-  const image = document.createElement("img");
-  const name = document.createElement("h3");
-  const address = document.createElement("p");
-  const phone = document.createElement("p");
-  const website = document.createElement("a");
+    const image = document.createElement("img");
+    const name = document.createElement("h3");
+    const level = document.createElement("h4");
+    const address = document.createElement("p");
+    const phone = document.createElement("p");
+    const website = document.createElement("a");
 
-  image.src = business.image;
-  image.alt = business.name;
-  image.loading = "lazy";
-  image.width = 190;
-  image.height = 100;
+    image.src = business.image;
+    image.alt = business.name;
+    image.loading = "lazy";
+    image.width = 190;
+    image.height = 100;
 
-  name.textContent = business.name;
-  address.textContent = business.address;
-  phone.textContent = business.phone;
+    name.textContent = business.name;
+    address.textContent = business.address;
+    phone.textContent = business.phone;
 
-  website.href = business.website;
-  website.textContent = business.website;
-  website.target = "_blank";
+    website.href = business.website;
+    website.textContent = business.website;
+    website.target = "_blank";
+  
+    level.textContent = 
+        business.level === 3
+        ? "Gold Member"
+        : business.level === 2
+        ? "Silver Member"
+        : ""; 
 
-  card.append(image, name, address, phone, website);
+    card.append(image);
+    card.append(name);
 
-  return card;
+    if (level.textContent) {
+        card.append(level);
+    }
+
+    card.append(address, phone, website);
+
+    return card;
 }
