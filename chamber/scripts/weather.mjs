18a1e97e834042c1bf8407e2248d8cc20f2e0ba4
SarahMecham <sarahdmecham@gmail.com>
2025-11-12 17:14:20 -0700

    fixed an error

--- chamber/scripts/weather.mjs
+++ chamber/scripts/weather.mjs
@@ -3,7 +3,7 @@ import { loadFutureWeather } from "./forecast.mjs";
 
 const myKey = 'ddc4dcc33b1bba3709346d0e677f41a7';
 const lat = '40.56';
-const lon = '-112.01';
+const lon = '-111.93';
 
 loadCurrentWeather(lat, lon, myKey);
 loadFutureWeather(lat, lon, myKey);
