adff3df7b9d9846385541ce48859e81d50f75fdf
SarahMecham <sarahdmecham@gmail.com>
2025-11-18 16:49:38 -0700

    Finished thank you page

---
+++ chamber/scripts/thankyou.js
@@ -0,0 +1,11 @@
+const myInfo = new URLSearchParams(window.location.search);
+
+document.querySelector('#results').innerHTML = `
+  <div class="result-row"><span class="label">Name:</span><span class="value">${myInfo.get('firstName')} ${myInfo.get('lastName')}</span></div>
+  <div class="result-row"><span class="label">Title:</span><span class="value">${myInfo.get('title')}</span></div>
+  <div class="result-row"><span class="label">Email:</span><span class="value">${myInfo.get('email')}</span></div>
+  <div class="result-row"><span class="label">Phone:</span><span class="value">${myInfo.get('phone')}</span></div>
+  <div class="result-row"><span class="label">Business/Organization:</span><span class="value">${myInfo.get('orgName')}</span></div>
+  <div class="result-row"><span class="label">Membership Requested:</span><span class="value">${myInfo.get('membership')}</span></div>
+  <div class="result-row"><span class="label">Business/Organization Details:</span><span class="value">${myInfo.get('description')}</span></div>
+`;
