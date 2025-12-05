b7e15a40d1cd18069925f160ab739370f0421f7f
SarahMecham <sarahdmecham@gmail.com>
2025-12-05 16:26:25 -0700

    Finished my thank you page

--- project/scripts/thank-you.js
+++ project/scripts/thank-you.js
@@ -6,5 +6,6 @@ document.querySelector('#results').innerHTML = `
   <div class="result-row"><span class="label">Phone:</span><span class="value">${myInfo.get('phone')}</span></div>
   <div class="result-row"><span class="label">Family line:</span><span class="value">${myInfo.get('family')}</span></div>
   <div class="result-row"><span class="label">Question/Insight:</span><span class="value">${myInfo.get('question')}</span></div>
+  <div class="result-row"><span class="label">Story:</span><span class="value">${myInfo.get('story')}</span></div>
   <div class="result-row"><span class="label">Form Submitted On:</span><span class="value">${new Date(myInfo.get('timeStamp')).toLocaleString()}</span></div>
 `;
